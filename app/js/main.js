f77cbd5a04843d2aed9fe7ac45326210f4f87835
Maksym Dmitriiev <95755128+MaxDM2021@users.noreply.github.com>
2023-07-26 00:30:47 +0300

    1212

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,26 @@ $('.top__slider').slick({
 
 });
 
+
+$('.contact-slider').slick({
+    slidesToShow: 10,
+    slidesToScroll: 10,
+    dots: true,
+    arrows: false,
+
+});
+
+
+$('.article-slider__box').slick({
+prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow-left"></button>',
+nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt="arrow-right"></button>'
+
+
+
+});
+
+
+
 var mixer = mixitup('.gallery__inner', {
     load: {
         filter: '.living'
